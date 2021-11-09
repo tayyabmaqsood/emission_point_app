9e855d0b58f9c6b97b63875d2bcf4510677f3ae5
tayyabmaqsood48 <tayyab.maqsood@devsinc.com>
2021-11-09 20:24:38 +0500

    switch backend implementation done

--- app/src/main/java/com/example/emissionpointapp/MainActivity.java
+++ app/src/main/java/com/example/emissionpointapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.emissionpointapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void hideImageView(View view) {
+        ImageView imageView = findViewById(R.id.repoView);
+        if (imageView.getVisibility() == view.VISIBLE) {
+            imageView.setVisibility(view.INVISIBLE);
+        }
+        else
+            imageView.setVisibility(view.VISIBLE);
     }
 }
