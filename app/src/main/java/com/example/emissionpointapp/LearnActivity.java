38fec5e0686f61eed98f9a72fa06b361f97d6274
tayyabmaqsood48 <tayyab.maqsood@devsinc.com>
2021-11-11 02:12:33 +0500

    learn by alphabets activity completed

--- app/src/main/java/com/example/emissionpointapp/LearnActivity.java
+++ app/src/main/java/com/example/emissionpointapp/LearnActivity.java
@@ -3,6 +3,7 @@ package com.example.emissionpointapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.gridlayout.widget.GridLayout;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
@@ -22,35 +23,21 @@ public class LearnActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn);
-        char[] arabic_alpha = {'ا', 'ب',  'ت', 'ث', 'ج', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'س','ش','ص', 'ض', 'ط', 'ظ', 'ع', 'غ', 'ف', 'ق', 'ک', 'ل', 'م', 'ن', 'و', 'ه', 'ی'};
-        List<String> filename = Arrays.asList("mk123","mk1516","mk11","mk12","mk67","mk123","mk123","mk11","mk12","mk8910","mk13","mk13","mk67","mk13","mk67","mk11","mk12","mk123","mk123","mk1516","mk45","mk45","mk8910","mk1516","mk14","mk1516","mk123","mk67");
-        for (int i = 0; i < arabic_alpha.length; i++) {
-            GridLayout gridLayout = findViewById(R.id.gridLayout);
-            Button btn = new Button(this);
-            btn.setId(i);
-            btn.setText(String.valueOf(arabic_alpha[i]));
-            btn.setLayoutParams(new GridLayout.LayoutParams());
-            btn.setLayoutParams(new ViewGroup.LayoutParams(120, ViewGroup.LayoutParams.WRAP_CONTENT));
-            btn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    int id = v.getId();
-                    ImageView imageView = findViewById(R.id.resultImage);
-                    String uri = "@drawable/"+ filename.get(id);
-                    int imageResource = getResources().getIdentifier(uri, null, getPackageName());
-                    Drawable res = getResources().getDrawable(imageResource);
-                    imageView.setImageDrawable(res);
 
-                }
-            });
-            gridLayout.addView(btn);
-
-        }
     }
 
 
 
     public void endActivity(View view) {
+        finish();
+    }
+
+    public void learnByAlphabets(View view) {
+        Intent intent = new Intent(this, LearnByAlphabets.class);
+        startActivity(intent);
+    }
+
+    public void finishActivity(View view) {
         finish();
     }
 }
--- app/src/main/java/com/example/emissionpointapp/LearnByAlphabets.java
+++ app/src/main/java/com/example/emissionpointapp/LearnByAlphabets.java
@@ -1,8 +1,17 @@
 package com.example.emissionpointapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.gridlayout.widget.GridLayout;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class LearnByAlphabets extends AppCompatActivity {
 
@@ -10,5 +19,29 @@ public class LearnByAlphabets extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn_by_alphabets);
+        char[] arabic_alpha = {'ا', 'ب',  'ت', 'ث', 'ج', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'س','ش','ص', 'ض', 'ط', 'ظ', 'ع', 'غ', 'ف', 'ق', 'ک', 'ل', 'م', 'ن', 'و', 'ه', 'ی'};
+        List<String> filename = Arrays.asList("mk123","mk1516","mk11","mk12","mk67","mk123","mk123","mk11","mk12","mk8910","mk13","mk13","mk67","mk13","mk67","mk11","mk12","mk123","mk123","mk1516","mk45","mk45","mk8910","mk1516","mk14","mk1516","mk123","mk67");
+        for (int i = 0; i < arabic_alpha.length; i++) {
+            GridLayout gridLayout = findViewById(R.id.gridLayout);
+            Button btn = new Button(this);
+            btn.setId(i);
+            btn.setText(String.valueOf(arabic_alpha[i]));
+            btn.setLayoutParams(new GridLayout.LayoutParams());
+            btn.setLayoutParams(new ViewGroup.LayoutParams(120, ViewGroup.LayoutParams.WRAP_CONTENT));
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int id = v.getId();
+                    ImageView imageView = findViewById(R.id.resultImage);
+                    String uri = "@drawable/"+ filename.get(id);
+                    int imageResource = getResources().getIdentifier(uri, null, getPackageName());
+                    Drawable res = getResources().getDrawable(imageResource);
+                    imageView.setImageDrawable(res);
+
+                }
+            });
+            gridLayout.addView(btn);
+
+        }
     }
 }
