5aa5f7b6257bd209e446f92bf9bd2c8d4b6d0a23
tayyabmaqsood48 <tayyab.maqsood@devsinc.com>
2021-11-11 01:53:51 +0500

    learn activity completed

--- app/src/main/java/com/example/emissionpointapp/LearnActivity.java
+++ app/src/main/java/com/example/emissionpointapp/LearnActivity.java
@@ -3,12 +3,18 @@ package com.example.emissionpointapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.gridlayout.widget.GridLayout;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class LearnActivity extends AppCompatActivity {
 
@@ -16,24 +22,33 @@ public class LearnActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn);
-        char[] arabic_alpha = {'ا', 'ب', 'پ', 'ت', 'ث', 'ج', 'چ', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'ژ', 'ص', 'ض', 'ط', 'ظ', 'ع', 'غ', 'ف', 'ق', 'ک', 'گ', 'ل', 'م', 'ن', 'و', 'ه', 'ی'};
+        char[] arabic_alpha = {'ا', 'ب',  'ت', 'ث', 'ج', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'س','ش','ص', 'ض', 'ط', 'ظ', 'ع', 'غ', 'ف', 'ق', 'ک', 'ل', 'م', 'ن', 'و', 'ه', 'ی'};
+        List<String> filename = Arrays.asList("mk123","mk1516","mk11","mk12","mk67","mk123","mk123","mk11","mk12","mk8910","mk13","mk13","mk67","mk13","mk67","mk11","mk12","mk123","mk123","mk1516","mk45","mk45","mk8910","mk1516","mk14","mk1516","mk123","mk67");
+        for (int i = 0; i < arabic_alpha.length; i++) {
+            GridLayout gridLayout = findViewById(R.id.gridLayout);
+            Button btn = new Button(this);
+            btn.setId(i);
+            btn.setText(String.valueOf(arabic_alpha[i]));
+            btn.setLayoutParams(new GridLayout.LayoutParams());
+            btn.setLayoutParams(new ViewGroup.LayoutParams(120, ViewGroup.LayoutParams.WRAP_CONTENT));
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int id = v.getId();
+                    ImageView imageView = findViewById(R.id.resultImage);
+                    String uri = "@drawable/"+ filename.get(id);
+                    int imageResource = getResources().getIdentifier(uri, null, getPackageName());
+                    Drawable res = getResources().getDrawable(imageResource);
+                    imageView.setImageDrawable(res);
 
-        GridLayout gridLayout = findViewById(R.id.gridLayout);
-        Button btn = new Button(this);
-        btn.setId(arabic_alpha[0]);
-        btn.setText(arabic_alpha[0]);
-        gridLayout.addView(btn);
+                }
+            });
+            gridLayout.addView(btn);
 
-        //        for (int i = 0; i < arabic_alpha.length; i++) {
-//            Button btn = new Button(this);
-//            btn.setId(arabic_alpha[i]);
-//            btn.setText(arabic_alpha[i]);
-//            btn.setHeight(100);
-//            btn.setWidth(100);
-//            btn.setLayoutParams(new GridLayout.LayoutParams());
-//
-//        }
+        }
     }
+
+
 
     public void endActivity(View view) {
         finish();
