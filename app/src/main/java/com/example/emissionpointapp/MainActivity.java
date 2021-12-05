80dc59914ba04e7b81aa7fba8ebe9e1c1b4b72f8
tayyabmaqsood48 <tayyab.maqsood@devsinc.com>
2021-12-06 01:28:32 +0500

    menu attach to toolbar

--- app/src/main/java/com/example/emissionpointapp/MainActivity.java
+++ app/src/main/java/com/example/emissionpointapp/MainActivity.java
@@ -1,21 +1,56 @@
 package com.example.emissionpointapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+//import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //findViewById(R.id.backIcon).setVisibility(View.INVISIBLE);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+//        return super.onCreateOptionsMenu(menu);
+        return  true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.Practice:
+                    Intent intent = new Intent(this, LearnActivity.class);
+                    startActivity(intent);
+            case R.id.Exam:
+                intent = new Intent(this, ExamActivity.class);
+                startActivity(intent);
+            case R.id.BackMenu:
+                finish();
+            case R.id.Quit:
+                finishAndRemoveTask();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     public void openRepository(View view) {
         Uri uri = Uri.parse("https://github.com/tayyabmaqsood/emission_point_app"); // missing 'http://' will cause crashed
@@ -26,5 +61,9 @@ public class MainActivity extends AppCompatActivity {
     public void openSecondActivity(View view) {
         Intent intent = new Intent(MainActivity.this, MainActivity2.class);
         startActivity(intent);
+    }
+
+    public void backIconClicked(View view){
+        finish();
     }
 }
